3df6fcd4ff912b7141b4c7f852ca92bfe3ae427a
iamDera <deraanagor@gmail.com>
2022-02-14 14:31:25 +0100

    first commit

---
+++ my-first-async-io.js
@@ -0,0 +1,6 @@
+let fs = require("fs");
+let file = process.argv[2];
+fs.readFile(file, function (err, contents) {
+  let lines = contents.toString().split("\n").length - 1;
+  console.log(lines);
+});
